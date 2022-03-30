fab79be333b93f3f2037838d8e324be8d4d1ebcf
Zacchaeus14 <yw3642@nyu.edu>
2022-03-30 10:37:57 +0800

    init

---
+++ server.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from sanic import Sanic
+from sanic.response import json
+
+DATA = pd.read_parquet('data/ipums_full_count_nyc_census_coded_20210801.parquet')
+DATA['SINCEIMMIG'] = DATA['YEAR'] - DATA['YRIMMIG']
+DATA['AGEIMMIG'] = DATA['AGE'] + DATA['YEAR'] - DATA['YRIMMIG']
+
+app = Sanic("HRLInteractivePortal")
+
+
+@app.get("/")
+async def hello_world(request):
+    return json({"Hello": "world."})
+
+
+@app.get("/card")
+async def car(request):
+    """
+    cardinality
+    :param request: x, variable to return cardinality
+    :return: [x_1, x_2, ..., x_n]
+    """
+    x = request.args.get('x')
+    if not x:
+        return json([], status=400)
+    x = x.upper()
+    return json(DATA[x].unique().tolist())
+
+@app.get("/bar")
+async def bar(request):
+    """
+    bar chart
+    :param request: x, variable on the x-axis
+    :param request: group, variable on which x is grouped (default 'all')
+    :return:{'group1':
+                {x: [], count: []},
+            'group2': {...}
+            ...
+            }
+    """
+    x = request.args.get('x')
+    if not x:
+        return json({}, status=400)
+    x = x.upper()
+    group = request.args.get('group')
+    if not group:
+        values, counts = np.unique(DATA[x].values, return_counts=True)
+        values = values.tolist()
+        counts = counts.tolist()
+        return json({'all': {'x': values, 'y': counts}})
+    group = group.upper()
+    response = {}
+    for g in np.unique(DATA[group].values):
+        subdf = DATA[DATA[group] == g]
+        values, counts = np.unique(subdf[x].values, return_counts=True)
+        values = values.tolist()
+        counts = counts.tolist()
+        response[g] = {'x': values, 'y': counts}
+    return json(response)
