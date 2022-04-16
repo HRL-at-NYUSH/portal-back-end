015ebf2e7610bb091e2fa2e9abd4650de79176d8
Zacchaeus14 <yw3642@nyu.edu>
2022-04-16 21:55:52 +0800

    add line chart

--- server.py
+++ server.py
@@ -57,7 +57,7 @@ async def columns(request):
     available columns
     :return: [col_0, col_1, ...]
     """
-    return sanic.response.json(DATA.columns.values.tolist())
+    return sanic.response.json(DATA.columns.tolist())
 
 
 @app.get("/card")
@@ -81,7 +81,7 @@ async def bar(request):
     :param request: x, variable on the x-axis
     :param request: group, variable on which x is grouped (default 'all')
     :return:{group0:
-                {"x": [x_0, x_1, ...], "y": [count(x_0; group0), count(x_1; group0), ...]},
+                {"x": [x_0, x_1, ..., x_{n_cat-1}], "y": [count(x_0; group0), count(x_1; group0), ..., count(x_{n_cat-1}; group0)]},
             group1: {...}
             ...
             }
@@ -105,7 +105,40 @@ async def bar(request):
     groups = DATA.groupby([group, x]).size()
     for g in groups.index.get_level_values(0).unique():
         subdf = groups[g]
-        response[g] = {"x": subdf.index.values.tolist(), "y": subdf.values.tolist()}
+        response[g] = {"x": subdf.index.tolist(), "y": subdf.tolist()}
+    return sanic.response.json(response)
+
+
+@app.get("/line")
+async def line(request):
+    """
+    line chart
+    :param request: y, variable on the y-axis (x-axis is YEAR)
+    :param request: group, variable on which y is grouped (default 'all')
+    :return:{group0:
+                {"x": [year_0, year_1, ..., y_{n_year-1}], "y": [y_0.mean, y_1.mean, ..., y_{n_year-1}.mean]},
+            group1: {...}
+            ...
+            }
+    """
+    y = request.args.get('y')
+    if not y:
+        return sanic.response.json({}, status=400)
+    y = y.upper()
+    if y not in CONST['variables']['line']:
+        return sanic.response.json({}, status=400)
+    group = request.args.get('group')
+    if not group or group.upper() == 'YEAR':
+        subdf = DATA.groupby('YEAR')[y].mean()
+        return sanic.response.json({'all': {'x': subdf.index.tolist(), 'y': subdf.tolist()}})
+    group = group.upper()
+    if group not in CONST['filters'].get(y, []):
+        return sanic.response.json({}, status=400)
+    response = {}
+    groups = DATA.groupby([group, 'YEAR'])[y].mean()
+    for g in groups.index.get_level_values(0).unique():
+        subdf = groups[g]
+        response[g] = {'x': subdf.index.tolist(), 'y': subdf.tolist()}
     return sanic.response.json(response)
 
 
